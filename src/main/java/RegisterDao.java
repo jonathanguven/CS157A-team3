ea38e8e10b76ac3e406b2ca1f9fbc02e7119d28a
Jimmy Vu <jimmyvu1357@gmail.com>
2024-10-15 00:41:16 -0700

    Member.java: added role setter and getter RegisterDao.java: handles inserting admin into DB Register.java: added role attribute +adminRegister.jsp: form for admin account creation +AdminRegister.java: servlet to handle admin account creation

--- src/main/java/RegisterDao.java
+++ src/main/java/RegisterDao.java
@@ -1,49 +1,49 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class RegisterDao {
-	private String dburl = "jdbc:mysql://localhost:3306/Nguyen";
-	private String dbuname = "root";
-	private String dbpassword = "password";
-	private String dbdriver = "com.mysql.cj.jdbc.Driver";
-	
-	public void loadDriver(String dbDriver) {
-		try {
-			Class.forName(dbDriver);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public Connection getConnection() {
-		Connection con = null;
-		try {
-			con = DriverManager.getConnection(dburl, dbuname, dbpassword);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return con;
-	}
-	
-	public String insert(Member member) {
-		loadDriver(dbdriver);
-		Connection con = getConnection();
-		String res = "Data entered successfully";
-		String sql = "insert into member values(?,?,?,?)";
-		try {
-			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1, member.getUname());
-			ps.setString(2, member.getPassword());
-			ps.setString(3, member.getEmail());
-			ps.setString(4, member.getPhone());
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			res = "Data not entered";
-		}
-		return res;
-	}
+    private String dburl = "jdbc:mysql://localhost:3306/vu";
+    private String dbusername = "root";
+    private String dbpassword = "password";
+    private String dbdriver = "com.mysql.jdbc.Driver";
+
+    public void loadDriver(String dbDriver) {
+        try {
+            Class.forName(dbDriver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(dburl, dbusername, dbpassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public String insert(Member member) {
+        loadDriver(dbdriver);
+        Connection con = getConnection();
+        String result = "Data entered successfully";
+        String sql = "INSERT INTO member (username, password, email, phone, role) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, member.getUsername());
+            ps.setString(2, member.getPassword());
+            ps.setString(3, member.getEmail());
+            ps.setString(4, member.getPhone());
+            ps.setString(5, member.getRole());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            result = "Data not entered";
+        }
+        return result;
+    }
 }
